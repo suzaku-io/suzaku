c1be395b44dc9bd4a0d55ad491fd6e243539fdf0
Thomas SIMON <thomas.simon.work@gmail.com>
2017-06-09 00:25:11 +0200

    Add Checkbox widget : definition and web implementation
    Checkbox without group policy (XOR, multiselect)

--- platform/web/base-widgets/src/main/scala/suzaku/platform/web/widget/package.scala
+++ platform/web/base-widgets/src/main/scala/suzaku/platform/web/widget/package.scala
@@ -9,6 +9,7 @@ package object widget {
   def registerWidgets(widgetManager: WidgetManager): Unit = {
     widgetManager.registerWidget(classOf[LinearLayout.WBlueprint], new DOMLinearLayoutBuilder(widgetManager))
     widgetManager.registerWidget(classOf[Button.WBlueprint], new DOMButtonBuilder(widgetManager))
+    widgetManager.registerWidget(classOf[Checkbox.WBlueprint], new DOMCheckboxBuilder(widgetManager))
     widgetManager.registerWidget(classOf[ListView.WBlueprint], new DOMListViewBuilder(widgetManager))
     widgetManager.registerWidget(classOf[Text.TextBlueprint], new DOMTextBuilder(widgetManager))
     widgetManager.registerWidget(classOf[TextInput.WBlueprint], new DOMTextInputBuilder(widgetManager))
